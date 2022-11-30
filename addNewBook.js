31543a1edeeda496b2a492f4b0ee84bad8da70c3
Taha Jumaah <taharasheed95@gmail.com>
2022-11-30 17:39:27 +0100

    Most of the logic is done, working on adding the title and details, delete works now.

--- addNewBook.js
+++ addNewBook.js
@@ -10,37 +10,28 @@ function onLoadFunction() {
     console.table(bookArray);
 }
 
-
 // End of Data Retreival
 
-// This section is for the main page
-let modalContainer = document.getElementById('main-container-modal');
+
+// This section is for showing and hiding the Modal to add a book.
 let addBookButtonMain = document.getElementById('add-button');
+let addBookDialog = document.getElementById('add-book-dialog');
 
-addBookButtonMain.addEventListener('pointerdown' , addBookFunctionMain);
+addBookButtonMain.addEventListener('pointerdown' , showDialog);
 
-function addBookFunctionMain() {
-    modalContainer.style.display = 'block';
-}
+function showDialog() {
+    addBookDialog.showModal();
+};
 
+let closeButton = document.getElementById('close');
 
-
-
-
-
-
+closeButton.addEventListener('pointerdown' , () => {addBookDialog.close()});
 
 
 
+// This section for adding book data to the Array after successful form submission.
 
-
-
-
-
-
-
-
-let addButton = document.getElementById('add-button-modal');
+let addBookForm = document.getElementById('add-book-form');
 let bookArray = [];
 let bookName = '';
 let author = '';
@@ -48,7 +39,7 @@ let pages = 0;
 let year = 0;
 let read = true;
 
-addButton.addEventListener('pointerdown', addBookFunction);
+addBookForm.addEventListener('submit', addBookFunction);
 
 class book {
     constructor(name, author, pages, year, read) {
@@ -67,5 +58,80 @@ function addBookFunction() {
     year = document.getElementById('year').value;
     let newBook = new book(bookName , author , pages , year);
     bookArray.push(newBook);
-    localStorage.setItem('test' , JSON.stringify(bookArray));
+    // localStorage.setItem('test' , JSON.stringify(bookArray));
 }
+
+
+
+// This Section is for appending Cards from the stored Array.
+let bookNameDiv = document.getElementsByTagName('h2');
+
+
+let body = document.getElementById('delete');
+body.addEventListener('click' , testFunction);
+
+let mainContainer = document.getElementById('main');
+
+
+let deleteButton = '';
+// deleteButton.addEventListener('pointerdown' , () => {this.parentNode.remove()})
+
+
+function testFunction() {
+    for (let index = 0; index < bookArray.length; index++) {
+
+        const book = bookArray[index];
+
+        let newBookDiv = document.createElement('div');
+        newBookDiv.className = 'card';
+        mainContainer.appendChild(newBookDiv);
+
+
+        let newBookTitleDiv = document.createElement('div');
+        newBookTitleDiv.className = 'book-title'
+        newBookDiv.appendChild(newBookTitleDiv);
+
+
+        let newBookTitle = document.createElement('h2');
+        newBookTitle.textContent = book.name;
+        newBookTitleDiv.appendChild(newBookTitle);
+
+        let newDeleteButton = document.createElement('button');
+        newDeleteButton.className = 'delete-button'
+        newBookDiv.appendChild(newDeleteButton);
+        this.parentNode.remove();
+
+        deleteButton = document.getElementsByClassName('delete-button');
+
+        newDeleteButton.addEventListener('click' , deleteFunction);
+    }
+
+    };
+
+
+    function deleteFunction() {
+        this.parentNode.remove();
+        console.log(this.parentNode.innerText);
+        for (let index = 0; index < bookArray.length; index++) {
+            const book = bookArray[index];
+            console.log(book.name);
+            if (this.parentNode.innerText == book.name) {
+                localStorage.setItem('test' , JSON.stringify(bookArray));
+
+                bookArray.splice(index, 1)
+                console.log('I deleted Something from the Array')
+                console.table(bookArray);
+                
+            }
+            
+            
+        }
+        
+    }
+
+
+
+    window.onbeforeunload = function(){
+        localStorage.setItem('test' , JSON.stringify(bookArray));
+
+    }
